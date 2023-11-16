de071be62608fe512682000b7a20c8e0c473ea28
Loek-Kai <94299450+Loek-Kai@users.noreply.github.com>
2023-11-16 12:33:54 +0100

    Day/Night Cycle Finished

---
+++ Assets/Loek/LightingManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    //References
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private LightingPreset preset;
+    //Variables
+    [SerializeField, Range(0, 24)] private float timeOfDay;
+
+    private void Update()
+    {
+        if (preset==null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            timeOfDay += Time.deltaTime;
+            timeOfDay %= 24; //Clamp between 0-24
+            UpdateLighting(timeOfDay / 24);
+        }
+        
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = preset.ambientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = preset.fogColor.Evaluate(timePercent);
+
+        if (directionalLight != null)
+        {
+            directionalLight.color = preset.directionalColor.Evaluate(timePercent);
+            directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170, 0));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (directionalLight != null)
+            return;
+
+        if (RenderSettings.sun != null)
+        {
+            directionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    directionalLight = light;
+                    return;
+                }
+            }
+        }
+
+        
+    }
+}
