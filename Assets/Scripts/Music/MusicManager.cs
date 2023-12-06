60f8220fcbc6502486eb0b6f4529ea801cd69e91
Julian-Tom <210401@student.glu.nl>
2023-12-06 13:03:34 +0100

    a random song gets selected from a list depending on day or night

---
+++ Assets/Scripts/Music/MusicManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] List<AudioSource> dayMusic;
+    [SerializeField] List<AudioSource> nightMusic;
+
+    [SerializeField] private float minimumWait;
+    [SerializeField] private float maximumWait;
+    public bool day;
+    void Start()
+    {
+        StartCoroutine(MusicTimer());
+    }
+
+    private IEnumerator MusicTimer()
+    {
+        float songLength = 0;
+        while(true)
+        {
+        yield return new WaitForSeconds(Random.Range(minimumWait, maximumWait) + songLength);
+
+        int index;
+        switch(day)
+        {
+            case true:
+                index = Random.Range(0,dayMusic.Count);
+                dayMusic[index].Play();
+                songLength = dayMusic[index].clip.length;
+                break;
+            case false:
+                index = Random.Range(0,nightMusic.Count);
+                nightMusic[index].Play();
+                songLength = nightMusic[index].clip.length;
+                break;
+        }
+        }
+
+        yield return null;
+    }
+}
