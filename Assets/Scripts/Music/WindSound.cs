560bea841c1cdfa8ae41622227c131a60ffaa0a8
Julian-Tom <210401@student.glu.nl>
2023-12-06 15:17:48 +0100

    the wind now reacts to the camera's speed

--- Assets/Scripts/Music/WindSound.cs
+++ Assets/Scripts/Music/WindSound.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class WindSound : MonoBehaviour
 {
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float minPitch = 0.1f;
+    [SerializeField] private float maxPitch = 2f;
+    [SerializeField] private float minVolume = 0.1f;
+    [SerializeField] private float maxVolume = 1f;
+    private Transform camera;
+    private AudioSource wind;
+    private Vector3 previousPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        wind = GetComponent<AudioSource>();
+        camera = GetComponentInParent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float speed = (camera.position - previousPosition).magnitude / Time.fixedDeltaTime;
+        float speedPercentage = 0;
+        if(speed != 0)
+        {
+            speedPercentage = speed / (maxSpeed - minSpeed);
+
+        }
+        Debug.Log(speedPercentage);
+        wind.pitch = (maxPitch + minPitch) * speedPercentage;
+        wind.volume = (maxVolume + minVolume) * speedPercentage;
+        wind.pitch = Mathf.Clamp(wind.pitch, 0,maxPitch);
+        wind.volume = Mathf.Clamp(wind.pitch, 0,maxVolume);
+
+        previousPosition = camera.position;
+
     }
 }
