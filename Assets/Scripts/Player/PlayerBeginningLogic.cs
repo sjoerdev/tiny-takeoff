b8bdd78852aadee9bd3d8e871ac4565bbbb1a13d
Julian-Tom <210401@student.glu.nl>
2023-12-14 19:06:14 +0100

    fixed the wind sound

--- Assets/Scripts/Player/PlayerBeginningLogic.cs
+++ Assets/Scripts/Player/PlayerBeginningLogic.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class PlayerBeginningLogic : MonoBehaviour
