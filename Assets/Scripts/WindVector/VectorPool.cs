ccf71ccb3bf8230ceaa874343ced4a57180a2eb2
Loek-Kai <94299450+Loek-Kai@users.noreply.github.com>
2023-12-21 11:58:39 +0100

    Trail update

---
+++ Assets/Scripts/WindVector/VectorPool.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorPool : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    public int poolSize = 10;
+
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(objectPrefab);
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        if (pool.Count > 0)
+        {
+            GameObject obj = pool.Dequeue();
+            obj.SetActive(true);
+            return obj;
+        }
+        return null;
+    }
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
