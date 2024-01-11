a8c70ee636830ec11361d475b0a774c73e6fdc00
Julian-Tom <210401@student.glu.nl>
2024-01-11 18:19:16 +0100

    the skybox now changes at night

--- Assets/Scripts/Skybox/ChangeSkyBox.cs
+++ Assets/Scripts/Skybox/ChangeSkyBox.cs
@@ -6,16 +6,33 @@ public class ChangeSkyBox : MonoBehaviour
 {
     [SerializeField] private Material skyBox;
     private LightingManager lightingManager;
+    [SerializeField] private float maxTime = 600;
+    private float dayTime;
 
     // Start is called before the first frame update
     void Start()
     {
         lightingManager = GetComponentInParent<LightingManager>();
+        dayTime = maxTime/2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        skyBox.SetFloat("_CubemapTransition", 0.2314f);
+        float time = lightingManager.GetTimeOfDay();
+        float timePercent = 0f;
+        if(time >= dayTime)
+        {
+            time -= dayTime;
+            float adjustedMaxTime = maxTime - dayTime;
+            timePercent = time/adjustedMaxTime;
+        }
+        else
+        {
+            timePercent = time/dayTime;
+            timePercent *= -1f;
+            timePercent += 1f;
+        }
+        skyBox.SetFloat("_CubemapTransition", timePercent);
     }
 }
